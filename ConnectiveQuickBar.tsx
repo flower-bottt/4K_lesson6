2906048459529f9a325039f5fa4c3ca290b60ea4
flower-bottt <cor9858@ssj.sen.es.kr>
2026-07-25 15:15:33 +0900

    Update ConnectiveQuickBar.tsx

--- ConnectiveQuickBar.tsx
+++ ConnectiveQuickBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Sparkles } from 'lucide-react';
-import { playCyberSound } from '../utils/audio';
+import { playCyberSound } from './audio';
 
 interface ConnectiveQuickBarProps {
   onInsertConnective: (connective: string) => void;
